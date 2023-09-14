aa5a15434b51a15fa788cd7339a3fa7deb2e9ade
Jose Gil Varon Moreno <jgvm91@gmail.com>
2023-09-14 07:01:53 -0500

    agregando caracteristicas al form de colaborador. se agrega form para crear equipo

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,7 +1,8 @@
 import "./form.css";
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
-import CampoTexto from "../input/TextInput";
+import hexToRgba from "hex-to-rgba";
+import Campo from "../Campo/campo";
 import ListaOpciones from "../ListaOpciones";
 import Boton from "../Boton";
 
@@ -11,42 +12,61 @@ const Formulario = (props) => {
 	const [foto, actualizarFoto] = useState("");
 	const [equipo, actualizarEquipo] = useState("");
 
-	const { registrarColaborador } = props;
+	const [titulo, actualizarTitulo] = useState("");
+	const [color, actualizarColor] = useState("");
 
-	const manejarEnvio = (e) => {
+	const { registrarColaborador, crearEquipo } = props;
+
+	const manejarEnvioEmpleado = (e) => {
 		e.preventDefault();
 		// console.log("Evento activado", e);
 		let datosEmpleado = {
+			id: uuid(),
 			nombre,
 			puesto,
 			foto,
 			equipo,
-			id: uuid(),
+			fav: false,
 		};
 		registrarColaborador(datosEmpleado);
 	};
 
+	const manejarEnvioEquipo = (e) => {
+		e.preventDefault();
+		let datosEquipo = {
+			id: uuid(),
+			titulo,
+			colorPrimario: color,
+			colorSecundario: hexToRgba(color, 0.6),
+		};
+		// console.log(datosEquipo);
+		crearEquipo(datosEquipo);
+	};
+
 	return (
-		<section className="formulario flex justify-center my-20">
-			<form onSubmit={manejarEnvio} className="w-[80%] px-24 py-8 drop-shadow-md rounded-2xl">
+		<section className="formulario flex justify-evenly flex-wrap lg:flex-nowrap gap-10 my-20 mx-12">
+			{/* Form Colaborador */}
+			<form
+				onSubmit={manejarEnvioEmpleado}
+				className="lg:w-[50%] p-10 relative drop-shadow-md rounded-2xl">
 				<h2 className="text-3xl font-semibold pb-8 font-serif">
 					Rellena el formulario para agregar al colaborador.
 				</h2>
-				<CampoTexto
+				<Campo
 					titulo="Nombre"
 					placeholder="Ingrese el Nombre"
 					required
 					valor={nombre}
 					actualizarValor={actualizarNombre}
 				/>
-				<CampoTexto
+				<Campo
 					titulo="Puesto"
 					placeholder="Ingrese el Puesto"
 					required
 					valor={puesto}
 					actualizarValor={actualizarPuesto}
 				/>
-				<CampoTexto
+				<Campo
 					titulo="Foto"
 					placeholder="Ingresar Enlace de Foto"
 					required
@@ -60,6 +80,25 @@ const Formulario = (props) => {
 					equipos={props.equipos}
 				/>
 				<Boton texto="Crear" />
+			</form>
+			{/* Form Equipo */}
+			<form onSubmit={manejarEnvioEquipo} className="lg:w-[50%] p-10 relative drop-shadow-md rounded-2xl">
+				<h2 className="text-3xl font-semibold pb-8 font-serif">Rellena el formulario para agregar Equipo.</h2>
+				<Campo
+					titulo="Titulo"
+					placeholder="Ingrese el Titulo"
+					required
+					valor={titulo}
+					actualizarValor={actualizarTitulo}
+				/>
+				<Campo
+					type="color"
+					titulo="Color Primario"
+					required
+					valor={color}
+					actualizarValor={actualizarColor}
+				/>
+				<Boton texto="Registrar Equipo" />
 			</form>
 		</section>
 	);
